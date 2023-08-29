4dffba2c5eb2a02a2d9d6e3f9f90d5a8ef3e0c78
“Owusu <owusuachamfour@gmail.com>
2023-08-29 14:13:05 -0400

    gh-pages installed and package.json and vite.config.js revised to make Githubpages work

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/PerScholas-Week8Day3HW/",
   plugins: [react()],
-})
+});
